ee37622c2052388a5bac0e9189d9b6a4c15e1ff8
Puneet Saini <ps.puneetsaini888@gmail.com>
2026-04-05 22:48:15 +0530

    rebuild

--- frontend/src/services/otpService.js
+++
@@ -1,8 +0,0 @@
-import API from './authService'
-
-export const generateOTP = () => API.post('/otp/generate')
-
-export const verifyOTP = (code, rfidUid = null) =>
-  API.post('/otp/verify', { code, rfidUid })
-
-export const getCurrentOTP = () => API.get('/otp/current')
